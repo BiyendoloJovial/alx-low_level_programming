4471b85b737bf3db444d4b7c3b6dd3c60f8f11f4
BiyendoloJovial <blackyblackmen@gmail.com>
2022-03-14 13:46:42 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
-/**
- *  * main - Entry point
- *   *
- *    * print \"Programming is like building a multilingual puzzle
- *     */
+
+
 int main(void)
 {
-	    printf("with proper grammar, but the outcome is a piece of art,");
+	    printf("with proper grammar, but the outcome is a piece of art, ");
 	    return (0);
 }
