41b5cc6b9e1bdf569832da4172f84178060661b4
BiyendoloJovial <blackyblackmen@gmail.com>
2022-03-14 13:10:59 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-
-
-int main (void)
+/**
+ *  * main - Entry point
+ *   *
+ *    * print "Programming is like building a multilingual puzzle
+ *     */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	    puts("\"Programming is like building a multilingual puzzle");
+	    return (0);
 }
